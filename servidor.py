bc0cfd0d95be8425aa2eca7e9e286afd02f8b05b
FelipeBorraz <felipesouto2901@gmail.com>
2025-11-23 13:15:47 -0300

    Adicionei servidor web em Python

---
+++ servidor.py
@@ -0,0 +1,57 @@
+# Importa o módulo socket
+from socket import *
+import sys  # Necessário para encerrar o programa
+
+# Cria o socket TCP (orientado à conexão)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepara o socket do servidor
+#Fill in start
+serverPort = 6789                     # Porta escolhida
+serverSocket.bind(('', serverPort))   # Associa IP e porta ao socket
+serverSocket.listen(1)                # Escuta conexões; backlog = 1
+#Fill in end
+
+while True:
+    # Estabelece a conexão
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()   #Fill in start / end
+    
+    try:
+        # Recebe a mensagem do cliente (requisição HTTP)
+        message = connectionSocket.recv(1024).decode()      #Fill in start / end
+        
+        filename = message.split()[1]
+        f = open(filename[1:])
+        
+        # Lê o conteúdo do arquivo
+        outputdata = f.read()         #Fill in start / end
+        
+        # Envia a linha de status do cabeçalho HTTP
+        #Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        #Fill in end
+        
+        # Envia o conteúdo do arquivo ao cliente
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        
+        connectionSocket.send("\r\n".encode())
+
+        # Fecha a conexão com o cliente
+        connectionSocket.close()
+
+    except IOError:
+        # Envia mensagem de erro 404 se o arquivo não for encontrado
+        #Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
+        #Fill in end
+        
+        # Fecha o socket do cliente
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
+
+serverSocket.close()
+sys.exit()  # Encerra o programa
